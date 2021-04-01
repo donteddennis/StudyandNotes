e25487f98bcaeee42adeb4e91b50d11e1a69de00
donteddennis <donte.d.dennis@gmail.com>
2021-04-01 09:13:47 -0400

    adding in new files in relation to git and terraform learning

---
+++ Git/Git Commands.sh
@@ -0,0 +1,13 @@
+# Pulling a new repo
+git clone 'repo url'
+
+# Making a new branch
+git branch 'new branch name'
+
+# Checking branches
+git branch -a 
+
+# Swapping to new branch
+git checkout 'branch name from git branch -a command'
+
+#
