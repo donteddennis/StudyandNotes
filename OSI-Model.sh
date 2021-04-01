3ffc1ab720753f761abc87249ac3e24a8f503540
donteddennis <donte.d.dennis@gmail.com>
2021-04-01 14:02:24 -0400

    added in details on the OSI model

---
+++ OSI-Model.sh
@@ -0,0 +1,48 @@
+#1 - Physical layer
+Transmits raw bit stream over the physical medium
+The physical layer is responsible for the physical cable or wireless connection between network nodes. 
+It defines the connector, the electrical cable or wireless technology connecting the devices, 
+and is responsible for transmission of the raw data, which is simply a series of 0s and 1s, while taking care of bit rate control.
+
+#2 - Data Link Layer
+Define the format of data on the network 
+The data link layer establishes and terminates a connection between two physically-connected nodes on a network. 
+It breaks up packets into frames and sends them from source to destination. 
+This layer is composed of two parts—Logical Link Control (LLC), which identifies network protocols, 
+performs error checking and synchronizes frames, and Media Access Control (MAC) which uses MAC addresses 
+to connect devices and define permissions to transmit and receive data.
+
+#3 - Network Layer
+Decides which physical path the data will take
+The network layer has two main functions. 
+One is breaking up segments into network packets, and reassembling the packets on the receiving end. 
+The other is routing packets by discovering the best path across a physical network. 
+The network layer uses network addresses (typically Internet Protocol addresses) to route packets to a destination node.
+
+#4 - Transport Layer
+Transmits data using transmission protocols including tcp and udp
+The transport layer takes data transferred in the session layer and breaks it into “segments” on the transmitting end. 
+It is responsible for reassembling the segments on the receiving end, turning it back into data that can be used by the session layer. 
+The transport layer carries out flow control, sending data at a rate that matches the connection speed of the receiving device, 
+and error control, checking if data was received incorrectly and if not, requesting it again.
+
+#5 - Session Layer
+Maintains connections and is resposible for controlling ports and sessions
+The session layer creates communication channels, called sessions, between devices. 
+It is responsible for opening sessions, ensuring they remain open and functional while 
+data is being transferred, and closing them when communication ends. 
+The session layer can also set checkpoints during a data transfer—if the session is interrupted,
+devices can resume data transfer from the last checkpoint.
+
+#6 - Presentation Layer
+Ensures that data is in a usable format and is where data encryption occures
+The presentation layer prepares data for the application layer. 
+It defines how two devices should encode, encrypt, and compress data so it is received correctly on the other end. 
+The presentation layer takes any data transmitted by the application layer and prepares it for transmission over the session layer.
+
+#7 - Application Layer
+Human-computer interaction layer, where applications can access the network services
+The application layer is used by end-user software such as web browsers and email clients. 
+It provides protocols that allow software to send and receive information and present meaningful data to users. 
+A few examples of application layer protocols are the Hypertext Transfer Protocol (HTTP), File Transfer Protocol (FTP),
+ Post Office Protocol (POP), Simple Mail Transfer Protocol (SMTP), and Domain Name System (DNS).
